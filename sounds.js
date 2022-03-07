00a9e2153cdb5ca5f19da1b50ad29bc194dc1eb8
Sam Aldeguer <aldeguersam@gmail.com>
2022-03-06 22:38:51 -0600

    Add comments for methods

--- sounds.js
+++ sounds.js
@@ -1,15 +1,18 @@
+//plays sound of bullet hitting ship
 const hitSound = () => {
     let hit = new Audio('sea_explosion.wav');
     hit.loop = false;
     hit.play();
 }
 
+//plays sound of water splashing
 const missSound = () => {
     let miss = new Audio('water_splash.mp3');
     miss.loop = false;
     miss.play();
 }
 
+//plays sound of an error notification
 const doubleClick = () => {
   let dc = new Audio("alert.wav");
   dc.loop = false;
